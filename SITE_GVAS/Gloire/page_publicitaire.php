9ce4071146a0ca01d31dcbfb1d707e19761f6d90
El-Mavic <belvie@gmail.com>
2026-05-28 01:52:45 +0100

    La nuit1

--- SITE_GVAS/Gloire/page_publicitaire.php
+++ SITE_GVAS/Gloire/page_publicitaire.php
@@ -140,19 +140,12 @@ if (!isset($_SESSION['visite_' . $page])) {
                     <!-- POPUP -->
 
                     <div class="popup" id="popup">
-
-
-                        <div class="popup-top pap">
-                            <label class="pap" for="email">Abonnement</label>
-
-                            <span class="close " id="closePopup">
-                                ✖
-                            </span>
-                        </div>
                         <form action="../Traitement.php" method="POST">
                             <input type="hidden" name="form_type" value="abonnement">
+
                             <input class="up_top" type="email" name="email" id="email"
                                 placeholder="Entrez votre adresse mail" required>
+
                             <button class="down_bottom" type="submit" id="valider">
                                 Valider
                             </button>
