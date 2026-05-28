d947f183f90b21adadbcc1e81b773eca6cb36098
El-Mavic <belvie@gmail.com>
2026-05-28 01:16:06 +0100

    La nuit1

--- SITE_GVAS/delete.php
+++ SITE_GVAS/delete.php
@@ -10,6 +10,7 @@ if (isset($_GET['id']) && isset($_GET['type'])) {
     $tables = [
         'temoignage' => 'temoignages',
         'inscription' => 'inscriptions',
+        'abonnement' => 'abonnements',
         'contact' => 'utilisateurs'
     ];
 
@@ -17,24 +18,20 @@ if (isset($_GET['id']) && isset($_GET['type'])) {
 
         $table = $tables[$type];
 
-        // Vérifier si l'élément est déjà dans la corbeille
         $check = $pdo->prepare("SELECT deleted FROM $table WHERE id = ?");
         $check->execute([$id]);
         $item = $check->fetch();
 
         if ($item && $item['deleted'] == 1) {
-            // Suppression définitive
             $sql = "DELETE FROM $table WHERE id = ?";
             $stmt = $pdo->prepare($sql);
             $stmt->execute([$id]);
 
             header("Location: corbeille.php?delete=success&t=" . time());
         } else {
-            // Envoyer à la corbeille
             $sql = "UPDATE $table 
-                    SET deleted = 1,
-                        deleted_at = NOW()
-                    WHERE id = ?";
+        SET deleted = 1
+        WHERE id = ?";
 
             $stmt = $pdo->prepare($sql);
             $stmt->execute([$id]);
